5d8cd62dac63bb76e9b777ab78c39cd52702ddb6
pphatdev <pphat.kits@gmail.com>
2025-07-09 16:52:02 +0700

    refactor: Enhance UI components and improve loading states for recommended books

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,6 @@ const renderRecommendedBooks = async () => {
     const recommendedBooksContainer = document.getElementById('recommended-books');
     // set loading state
     recommendedBooksContainer.innerHTML = loadingCard({ length: 10 });
-
     const books = await fetch(new URL(`/api/v1/ebooks`, APP_API_URL))
         .then(response => response.json())
         .then(data => data.result)
@@ -18,8 +17,8 @@ const renderRecommendedBooks = async () => {
         });
 
     recommendedBooksContainer.innerHTML = Array.from(books).map(book => `
-        <li class="bg-card border shrink-0 w-32 @sm:w-44 overflow-hidden border-border/5 rounded">
-            <img src="${new URL(book.image, APP_API_URL)}" alt="${book.title}" class="max-sm:h-36 w-full object-cover aspect-[3/4]">
+        <li class="shrink-0 w-32 @sm:w-44 overflow-hidden rounded">
+            <img src="${new URL(book.image, APP_API_URL)}" alt="${book.title}" class="max-sm:h-36 border border-border/10 rounded-lg w-full object-cover aspect-[3/4]">
             <div>
                 <h3 class="font-semibold">${book.title}</h3>
                 <p class="text-xs">${book.author}</p>
@@ -32,62 +31,8 @@ const renderRecommendedBooks = async () => {
 const authors = [
     {
         id: 1,
-        name: 'Author 1',
-        image: 'https://picsum.photos/200/300?random=1',
-        followers: 10,
-    },
-    {
-        id: 2,
-        name: 'Author 1',
-        image: 'https://picsum.photos/200/300?random=1',
-        followers: 10,
-    },
-    {
-        id: 1,
-        name: 'Author 1',
-        image: 'https://picsum.photos/200/300?random=1',
-        followers: 10,
-    },
-    {
-        id: 2,
-        name: 'Author 1',
-        image: 'https://picsum.photos/200/300?random=1',
-        followers: 10,
-    },
-    {
-        id: 1,
-        name: 'Author 1',
-        image: 'https://picsum.photos/200/300?random=1',
-        followers: 10,
-    },
-    {
-        id: 2,
-        name: 'Author 1',
-        image: 'https://picsum.photos/200/300?random=1',
-        followers: 10,
-    },
-    {
-        id: 1,
-        name: 'Author 1',
-        image: 'https://picsum.photos/200/300?random=1',
-        followers: 10,
-    },
-    {
-        id: 2,
-        name: 'Author 1',
-        image: 'https://picsum.photos/200/300?random=1',
-        followers: 10,
-    },
-    {
-        id: 1,
-        name: 'Author 1',
-        image: 'https://picsum.photos/200/300?random=1',
-        followers: 10,
-    },
-    {
-        id: 2,
-        name: 'Author 1',
-        image: 'https://picsum.photos/200/300?random=1',
+        name: 'PPhat Author',
+        image: 'https://github.com/pphatdev.png',
         followers: 10,
     },
 ]
@@ -95,7 +40,7 @@ const authors = [
 const renderAuthors = () => {
     const authorsContainer = document.getElementById('authors');
     authorsContainer.innerHTML = authors.map(author => `
-        <li>
+        <li class=" shrink-0">
             <a href="" class="flex flex-col items-center">
                 <img src="${author.image}" alt="${author.name}" class="size-20 ring-2 ring-primary rounded-full mb-2">
                 <h3 class="text-sm font-medium">${author.name}</h3>
@@ -106,35 +51,24 @@ const renderAuthors = () => {
 };
 
 
-const newReleaseBooks = [
-    {
-        id: 1,
-        title: 'New Release Book',
-        authors: [
-            'Author One',
-            'Author Two',
-        ],
-        image: 'https://picsum.photos/200/300?random=1',
-    },
-    {
-        id: 2,
-        title: 'Another New Release',
-        authors: [
-            'Author Three',
-            'Author Four',
-        ],
-        image: 'https://picsum.photos/200/300?random=2',
-    }
-]
+const renderNewReleaseBooks = async () => {
 
-const renderNewReleaseBooks = () => {
+    const books = await fetch(new URL(`/api/v1/ebooks`, APP_API_URL))
+        .then(response => response.json())
+        .then(data => data.result)
+        .catch(error => {
+            console.error('Error fetching books:', error);
+            return [];
+        });
+
     const newReleaseBooksContainer = document.getElementById('new-release');
-    newReleaseBooksContainer.innerHTML = newReleaseBooks.map(book => `
-        <li>
-            <a href="">
-                <img src="${book.image}" alt="${book.title}" class="w-40 h-60 object-cover rounded-lg shadow-md">
-                <h3 class="mt-2 text-sm font-medium text-gray-900">${book.title}</h3>
-            </a>
+    newReleaseBooksContainer.innerHTML = books.map(book => `
+        <li class="shrink-0 w-32 @sm:w-44 overflow-hidden rounded">
+            <img src="${new URL(book.image, APP_API_URL)}" alt="${book.title}" class="max-sm:h-36 border border-border/10 rounded-lg w-full object-cover aspect-[3/4]">
+            <div>
+                <h3 class="font-semibold">${book.title}</h3>
+                <p class="text-xs">${book.author}</p>
+            </div>
         </li>
     `).join('');
 };
@@ -143,16 +77,10 @@ const renderNewReleaseBooks = () => {
 document.addEventListener('DOMContentLoaded', () => {
     // Initialize layout
     initializeLayout()
-
-
     initializeHero();
 
     // Render recommended books
     renderRecommendedBooks()
-
-    // Render authors
     renderAuthors();
-
     renderNewReleaseBooks()
-
 });
