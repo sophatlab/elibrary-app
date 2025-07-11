ef348036a138316973d8bb31cbcd2351f4d3f1a1
pphatdev <pphat.kits@gmail.com>
2025-07-11 19:51:57 +0700

    fix: Update 404 page title and content; adjust webpack configuration for catch-all routing

--- src/js/404.js
+++ src/js/404.js
@@ -20,8 +20,9 @@ document.addEventListener('DOMContentLoaded', () => {
                     <img src="${cover}" loading="lazy" alt="Hero Cover Image" class="h-full object-cover">
                 </div>
                 <div class="absolute flex justify-center bg-gradient-to-t inset-0 p-5 to-transparent from-background flex-col gap-4 items-center">
-                    <h1 class="text-3xl max-md:text-primary-foreground sm:text-4xl text-primary md:text-5xl font-bold">Oops! Error 404</h1>
-                    <p class="text-lg md:text-xl mb-8 text-foreground/50" aria-live="polite">The page you're looking for cannot be found</p>
+                    <h1 class="text-3xl max-md:text-primary-foreground sm:text-4xl mt-10 text-primary md:text-5xl font-bold">Oops! Error 404</h1>
+                    <p class="text-lg md:text-xl mb-8 text-foreground/50" aria-live="polite">Sorry! The page is not found.</p>
+                    <p class="text-lg md:text-xl mb-8 text-foreground/50" aria-live="polite">If you want to read more about my information please choose one of the buttons blew.</p>
                     <div class="flex flex-col sm:flex-row gap-2 justify-center">
                         <a href="/collection" class="button button-primary">
                             Back to Collection
