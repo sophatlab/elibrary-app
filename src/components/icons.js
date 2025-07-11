e378fed2a5bcd6647c5e8a904130faef8fb3b273
pphatdev <pphat.kits@gmail.com>
2025-07-11 10:58:57 +0700

    feat: Add Icons component with home, collection, and help SVGs

---
+++ src/components/icons.js
@@ -0,0 +1,30 @@
+import { cn } from "../libs/tailwind.js";
+
+export class Icons {
+    static home = className => (
+        `<svg viewBox="0 0 24 24" fill="currentColor" class="${cn("size-5", className)}">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M12.707 2.293l9 9c.63 .63 .184 1.707 -.707 1.707h-1v6a3 3 0 0 1 -3 3h-1v-7a3 3 0 0 0 -2.824 -2.995l-.176 -.005h-2a3 3 0 0 0 -3 3v7h-1a3 3 0 0 1 -3 -3v-6h-1c-.89 0 -1.337 -1.077 -.707 -1.707l9 -9a1 1 0 0 1 1.414 0m.293 11.707a1 1 0 0 1 1 1v7h-4v-7a1 1 0 0 1 .883 -.993l.117 -.007z" />
+        </svg>`
+    )
+
+    static collection = className => (
+        `<svg class="${cn("size-5", className)}" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M5 4m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" />
+            <path d="M9 4m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" />
+            <path d="M5 8h4" />
+            <path d="M9 16h4" />
+            <path d="M13.803 4.56l2.184 -.53c.562 -.135 1.133 .19 1.282 .732l3.695 13.418a1.02 1.02 0 0 1 -.634 1.219l-.133 .041l-2.184 .53c-.562 .135 -1.133 -.19 -1.282 -.732l-3.695 -13.418a1.02 1.02 0 0 1 .634 -1.219l.133 -.041z" />
+            <path d="M14 9l4 -1" />
+            <path d="M16 16l3.923 -.98" />
+        </svg>`
+    )
+
+    static help = className => (
+        `<svg class="${cn("size-5", className)}" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+            <path d="M12 9h.01" />
+            <path d="M11 12h1v4h1" />
+        </svg>`
+    )
+}
